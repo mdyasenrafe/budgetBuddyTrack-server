2cd303bbedcd080cb482c8202300a894d7c450cb
Md yasen rafe <mdyasenrafe@gmail.com>
2023-10-05 17:11:52 +0600

    delete some code

--- bbt-product-listing/App.js
+++ bbt-product-listing/App.js
@@ -1,6 +1,3 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
-import { AppRegistry } from "react-native";
 import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
 import ProductListing from "./src/screens/ProductListing";
 import { useFonts } from "expo-font";
