5ad7d3200a73f59b583c26245422b02be06d500c
Md yasen rafe <mdyasenrafe@gmail.com>
2024-02-24 00:55:14 +0600

    hashpassword concept added

--- src/app/module/user/user.controller.ts
+++ src/app/module/user/user.controller.ts
@@ -1,15 +1,25 @@
 import { Request, Response } from "express";
 import { createUser, loginUser } from "./user.service";
 import { UserDataType } from "./user.interface";
+import bcrypt from "bcrypt";
 
 export const registerUser = async (req: Request, res: Response) => {
   try {
-    const registrationData = req.body;
-    const newUser = await createUser(registrationData);
-    return res.status(201).json({
-      error: false,
-      data: newUser,
-      message: "User registered successfully.",
+    const registrationData: UserDataType = req.body;
+    const { email, password, name } = req.body;
+    if (email && password && name) {
+      let hashedPassword = await bcrypt?.hash(password, 10);
+      registrationData["password"] = hashedPassword;
+      const newUser = await createUser(registrationData);
+      return res.status(201).json({
+        error: false,
+        data: newUser,
+        message: "User registered successfully.",
+      });
+    }
+    return res.status(400).json({
+      error: true,
+      message: "Please provide an email address and password",
     });
   } catch (error) {
     return res.status(500).json({
@@ -24,8 +34,9 @@ export const signInUser = async (
 ): Promise<void> => {
   try {
     const signInData: UserDataType = req.body;
-
-    if (!signInData?.email) {
+    const { email, password } = signInData;
+    console.log(password);
+    if (!email || !password) {
       return res.status(400).json({
         error: true,
         message: "Please provide an email address.",
@@ -38,14 +49,21 @@ export const signInUser = async (
       return res.status(401).json({
         error: true,
         data: user,
-        message: "Password didn't match.",
+        message: "user didn't find.",
       });
     }
+    const ValidPassword = await bcrypt.compare(password, user?.password);
 
-    return res.status(200).json({
-      error: false,
-      data: user,
-      message: "User signed in successfully.",
+    if (ValidPassword) {
+      return res.status(200).json({
+        error: false,
+        data: user,
+        message: "User signed in successfully.",
+      });
+    }
+    return res.status(401).json({
+      error: true,
+      message: "password didn't match",
     });
   } catch (error) {
     return res.status(500).json({
