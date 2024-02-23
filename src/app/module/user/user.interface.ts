fe2566ab4841e90151ef49c5e04482462ba82fc0
Md yasen rafe <mdyasenrafe@gmail.com>
2024-02-23 19:40:12 +0600

    project change typescript to js

--- src/app/module/user/user.interface.ts
+++
@@ -1,7 +0,0 @@
-export interface UserDataType {
-  name: string;
-  email: string;
-  profilePicture?: string; // Optional since not always required
-  password: string;
-  status: "active" | "inactive";
-}
--- src/app/module/user/user.route.ts
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import { registerUser } from "./user.controller";
-
-const userRouter = express.Router();
-
-userRouter.post("/register", registerUser);
-
-export default userRouter;
