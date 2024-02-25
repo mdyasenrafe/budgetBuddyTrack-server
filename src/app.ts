07afaf3a16221ec798a212427420ce503d05131f
Md yasen rafe <mdyasenrafe@gmail.com>
2024-02-25 16:21:25 +0600

    base64 issue resolved

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 import express, { Application, Request, Response } from "express";
 import cors from "cors";
+import bodyParser from "body-parser";
 
 const app: Application = express();
 
@@ -9,8 +10,12 @@ import imageUploadRouter from "./app/module/config/imageUpload";
 
 // Middleware
 app.use(cors());
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json({ limit: "25mb" }));
+app.use(
+  express.urlencoded({ limit: "25mb", extended: true, parameterLimit: 50000 })
+);
+
+app.use(bodyParser.text({ limit: "200mb" }));
 
 // Test route
 app.get("/", (req: Request, res: Response) => {
