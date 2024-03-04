388779e5bd8cd935c90494e8c384ebddefe3e7d7
Md yasen rafe <mdyasenrafe@gmail.com>
2024-03-05 01:09:54 +0600

    create card logic changed

--- src/app/module/card/card.service.ts
+++ src/app/module/card/card.service.ts
@@ -2,12 +2,16 @@ import { ITransaction } from "../transaction/transaction.interface";
 import { ICardData } from "./card.interface";
 import CardModel from "./card.model";
 
-export const createCardInDB = async (cardData: ICardData) => {
+export const createCardInDB = async (
+  cardData: ICardData
+): Promise<ICardData> => {
   const query = { userId: cardData.userId };
   const update = { $set: cardData };
-  const options = { upsert: true };
-  const newCard = await CardModel.updateOne(query, update, options);
-  return newCard;
+  const options = { upsert: true, returnOriginal: false, new: true };
+
+  const result = await CardModel.findOneAndUpdate(query, update, options);
+
+  return result;
 };
 
 export const getCardDetailsFromDB = async (
